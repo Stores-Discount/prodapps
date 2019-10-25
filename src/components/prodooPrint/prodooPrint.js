a08146716e64f350eba2ae0b7a4fa26bd82f2ed4
hparfr <raph@hpar.fr>
2019-10-25 11:08:01 +0200

    Allow multi label printing

--- src/components/prodooPrint/prodooPrint.js
+++ src/components/prodooPrint/prodooPrint.js
@@ -4,13 +4,15 @@ angular.module('prodapps').provider('prodooPrint',[ function prodooPrintProvider
 
     this.$get = ['$http', 'prodooConfig', 'Beep', function ($http, prodooConfig, Beep) {
             return function (payload, qte) {
-                var req = {
-                    args: ['label', payload.label],
-                    kwargs: { options : { 'copies': (qte) ? qte : payload.quantities }}
-                };
-                Beep.play();
-                $http.post(prodooConfig.pyWebDriver+'/cups/printData', req);
-                console.log('print !!!!', payload);
+                payload._v.labels.forEach(function (label) {
+                    var req = {
+                        args: ['label', label],
+                        kwargs: { options : { 'copies': (qte) ? qte : payload.quantities }}
+                    };
+                    Beep.play();
+                    $http.post(prodooConfig.pyWebDriver+'/cups/printData', req);
+                    console.log('print !!!!', payload);
+                });
             };
     }];
 
